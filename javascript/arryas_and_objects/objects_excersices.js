e273f70c7d15b3c425c0bb6e3447a4878a3b5fb1
ziv221 <ziv.itzh@gmail.com>
2020-08-11 12:09:38 +0300

    changed files' names and finished excersice 2

---
+++ javascript/arryas_and_objects/objects_excersices.js
@@ -0,0 +1,21 @@
+//Spot Check 1
+
+// const car = {
+//     color: "green", 
+//     numWheels: 4, 
+//     isFancy: false
+// }
+// let isOrIsnt = (car.isFancy) ?  "is" : "isn't" ;
+// console.log(`The ${car.color} car has ${car.numWheels} and ${isOrIsnt} Fancy`);
+
+// Spot Check 2
+
+// const myObj = {
+//     item: "shoe", 
+//     toBeggining: false, 
+//     items: ["keys", "cloths", "cutlery"]
+// }
+
+// let operation = (myObj.toBeggining) ? "push" : "unshift";
+// eval(`myObj.items.${operation}(myObj.item);`);
+// console.log(myObj.items);
