9f6ef74d7d49fec21c8a01aea10bbc2d382c8029
ziv221 <ziv.itzh@gmail.com>
2020-08-11 18:51:00 +0300

    solved the excersices

--- javascript/arryas_and_objects/objects_apply_excercises.js
+++ javascript/arryas_and_objects/objects_apply_excercises.js
@@ -15,18 +15,18 @@
 
 // Exercise 2
 
-myList = [
-    {
-        name: "Tofy", 
-        breed: "Terrier & Schnauzer",
-        isAlive: false
-    },
-    {
-        name: "Tito", 
-        breed: "Miniature Pinscher",
-        isAlive: true
-    }
-];
+// myList = [
+//     {
+//         name: "Tofy", 
+//         breed: "Terrier & Schnauzer",
+//         isAlive: false
+//     },
+//     {
+//         name: "Tito", 
+//         breed: "Miniature Pinscher",
+//         isAlive: true
+//     }
+// ];
 
 // myList[0].color = "cream";
 // myList.pop();
@@ -60,3 +60,56 @@ myList = [
 
 // myList.push(library);
 // console.log(myList);
+
+//Exercise 5
+// const reservations = {
+//     bob: { claimed: false },
+//     ted: { claimed: true }
+//   }
+  
+// let name = prompt('Please enter the name for your reservation');
+// name = name.toLowerCase();
+
+// if(name in reservations) {
+//     if(reservations[name].claimed){
+//         alert("You're reservation has been claimed already");
+//     } else {
+//         alert(`Welcome ${name}! `);
+//     }
+// } else {
+//     reservations[name] = {cliamed:true}
+//     alert("There's no existing resrvation");
+//     console.log(reservations);
+// }
+
+//Exercise 5.1 - Done
+//Exercise 5.2 - Done
+
+//Extension
+
+const date = 3
+
+const kitchen = {
+    owner: "Geraldine",
+    hasOven: false,
+    fridge: {
+        price: 500,
+        works: true,
+        items: [
+            { name: "cheese", expiryDate: 7 },
+            { name: "raddish", expiryDate: 2 },
+            { name: "bread", expiryDate: 1 }
+        ]
+    }
+}
+const hasOven = kitchen.hasOven;
+const fridgeWorks = kitchen.fridge.works;
+if( hasOven && fridgeWorks){
+    
+} else if(hasOven){
+    
+} else if(fridgeWorks){
+    
+} else {
+    
+}
---
+++ javascript/devTools/script.js
@@ -0,0 +1,15 @@
+let frontEnd = ['HTML', 'CSS', 'Javascript'];
+let counter = 100;
+let frontEndLangages = frontEnd;
+
+add = function (){
+    getOne() + getTwo();
+}
+getOne = function (){
+    return 1;    
+}
+getTwo = function (){
+    return 2;
+}
+
+add();
